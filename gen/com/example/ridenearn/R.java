9741b47a2a5353ef1f5bcadd656efc327952fb81
sankalp-chugh <sankalp.chugh@gmail.com>
2014-09-02 18:45:05 +0530

    layouts
    all layouts have been created

--- gen/com/example/ridenearn/R.java
+++ gen/com/example/ridenearn/R.java
@@ -1077,7 +1077,14 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int avatar=0x7f020057;
+        public static final int bike_icon=0x7f020058;
+        public static final int bike_logo=0x7f020059;
+        public static final int car_icon=0x7f02005a;
+        public static final int green_bike_icon=0x7f02005b;
+        public static final int green_car_icon=0x7f02005c;
+        public static final int ic_launcher=0x7f02005d;
+        public static final int star=0x7f02005e;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,25 +1098,43 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050063;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
+        public static final int avatar=0x7f050051;
         public static final int beginning=0x7f050011;
+        public static final int bikebtn=0x7f050053;
+        public static final int bikebtn2=0x7f050059;
+        public static final int button1=0x7f050050;
+        public static final int button2=0x7f050061;
+        public static final int button3=0x7f050062;
+        public static final int carbtn=0x7f050052;
+        public static final int carbtn2=0x7f050058;
+        public static final int checkBox1=0x7f050056;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int datePicker1=0x7f05005e;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editText1=0x7f050041;
+        public static final int editText2=0x7f050043;
+        public static final int editText3=0x7f05005d;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
+        public static final int expandableListView1=0x7f05005c;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f050049;
+        public static final int imageView2=0x7f05004d;
+        public static final int liftbtn=0x7f05003e;
+        public static final int linearLayout1=0x7f05003f;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1119,6 +1144,16 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int radioButton1=0x7f050044;
+        public static final int radioButton2=0x7f050045;
+        public static final int radioButton3=0x7f050047;
+        public static final int radioButton4=0x7f050046;
+        public static final int radioButton5=0x7f050048;
+        public static final int ratingBar1=0x7f05004f;
+        public static final int ratingBar2=0x7f05004b;
+        public static final int ridebtn=0x7f05003d;
+        public static final int ridenearnlogo=0x7f05003c;
+        public static final int ridersubmit=0x7f050060;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1136,10 +1171,21 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050040;
+        public static final int textView2=0x7f050042;
+        public static final int textView3=0x7f05004c;
+        public static final int textView4=0x7f05004e;
+        public static final int textView5=0x7f05004a;
+        public static final int textView6=0x7f050055;
+        public static final int textView7=0x7f050057;
+        public static final int textView8=0x7f05005a;
+        public static final int timePicker1=0x7f05005f;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int vehicle1=0x7f050054;
+        public static final int vehicle2=0x7f05005b;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1193,10 +1239,17 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_home=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_lift=0x7f030019;
+        public static final int activity_profile=0x7f03001a;
+        public static final int activity_profile_edit=0x7f03001b;
+        public static final int activity_register=0x7f03001c;
+        public static final int activity_riders=0x7f03001d;
+        public static final int activity_search=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
         public static final int home=0x7f0c0000;
+        public static final int lift=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1242,6 +1295,12 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_lift=0x7f0a0010;
+        public static final int title_activity_profile=0x7f0a0012;
+        public static final int title_activity_profile_edit=0x7f0a0011;
+        public static final int title_activity_register=0x7f0a0013;
+        public static final int title_activity_rider=0x7f0a0014;
+        public static final int title_activity_search=0x7f0a0015;
     }
     public static final class style {
         /** 
---
+++ src/com/example/ridenearn/ProfileEdit.java
@@ -0,0 +1,16 @@
+package com.example.ridenearn;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ProfileEdit extends ActionBarActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile_edit);
+	}
+	
+}
